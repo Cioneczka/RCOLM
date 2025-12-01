8239494a7b682cd8ef05ac0cf40e6296836a9d7e
Cioneczka <trzcionka.luk@gmail.com>
2025-12-01 14:32:04 +0100

    Stupid azure

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from app.routes import bp
+
 
 def create_app():
-    app = Flask(__name__)
-
-    # import tras z osobnego pliku
-    from . import routes
-    app.register_blueprint(routes.bp)
-
+    app = Flask(
+        __name__,
+        template_folder="app/templates",
+        static_folder="app/static",
+    )
+    app.register_blueprint(bp)
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
     return app
+
+
+# To jest obiekt, którego szuka gunicorn: app:app
+app = create_app()
 
