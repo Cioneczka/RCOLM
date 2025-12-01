d4cf30972cb486498a5db2d2198f5e777836a5bf
Cioneczka <trzcionka.luk@gmail.com>
2025-12-01 15:05:22 +0100

    end my suffering

--- data/static_data_collectors/data_extractors.py
+++ data/static_data_collectors/data_extractors.py
@@ -1,6 +1,4 @@
 import librosa
-import essentia
-import essentia.standard as es
 class Extractors:
     @staticmethod
     def tempo_estimator(file_path):
@@ -8,15 +6,15 @@ class Extractors:
         onset_env = librosa.onset.onset_strength(y=y, sr=sr)
         tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
         return tempo, sr
-
-    @staticmethod 
-    def key_extractor(file_path): 
-        
-        audio = es.MonoLoader(filename=file_path)()
-        
-        # Initialize KeyExtractor   
-        key_extractor = es.KeyExtractor()
-        key, scale, strength = key_extractor(audio)
-            
-        
-        return key, scale
+# 
+#     @staticmethod 
+#     def key_extractor(file_path): 
+#         
+#         audio = es.MonoLoader(filename=file_path)()
+#         
+#         # Initialize KeyExtractor   
+#         key_extractor = es.KeyExtractor()
+#         key, scale, strength = key_extractor(audio)
+#             
+#         
+#         return key, scale
