4257e2a68bd5d6f3fa5b0b140bcfc195cbe143e2
Cioneczka <trzcionka.luk@gmail.com>
2025-12-01 14:50:12 +0100

    i hate my life

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
+from app import app  # importuje app z app/__init__.py
 
-from flask import Flask
-from app.routes import bp
-
-
-app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
-app.register_blueprint(bp)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,12 @@ from app.routes import bp
 
 
 def create_app():
-    app = Flask(
-        __name__,
-        template_folder="app/templates",
-        static_folder="app/static",
-    )
+    app = Flask(__name__)
     app.register_blueprint(bp)
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     return app
 
 
-# To jest obiekt, którego szuka gunicorn: app:app
+# To jest obiekt, którego szuka gunicorn przy "app:app"
 app = create_app()
 
